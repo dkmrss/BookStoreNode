745849492fb59c1344d835776d991515f2f1c97a
ryuumugen <thangcp8@gmail.com>
2024-06-06 12:39:11 +0700

    update 1.0

--- src/app.js
+++ src/app.js
@@ -1,13 +1,46 @@
+require("dotenv").config();
+const createError = require("http-errors");
 const express = require("express");
-const systemConfig = require("./app/configs/system");
-const indexRouter = require("./app/routers/index");
+const dbConnection = require("./app/configs/database");
 
 const app = express();
-
 app.use(express.json());
 
-app.use("/", indexRouter);
+const pathConfig = require("./path");
+global.__base = __dirname + "/";
+global.__path_app = __base + pathConfig.folder_app + "/";
 
-app.listen(systemConfig.port, () => {
-  console.log(`Server is running on port ${systemConfig.port}`);
+global.__path_schemas = __path_app + pathConfig.folder_schemas + "/";
+global.__path_models = __path_app + pathConfig.folder_models + "/";
+global.__path_routers = __path_app + pathConfig.folder_routers + "/";
+global.__path_configs = __path_app + pathConfig.folder_configs + "/";
+
+const systemConfig = require(__path_configs + "system");
+const dbConfig = require(__path_configs + "database");
+
+app.locals.systemConfig = systemConfig;
+
+// Kết nối cơ sở dữ liệu
+dbConnection.connect((err) => {
+  if (err) {
+    console.error("Kết nối thất bại:", err.message);
+    return;
+  }
+  console.log("Kết nối thành công đến MySQL");
 });
+
+app.use("/api/v1/", require(__path_routers));
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.end("Lỗi, URL không đúng");
+});
+
+module.exports = app;
