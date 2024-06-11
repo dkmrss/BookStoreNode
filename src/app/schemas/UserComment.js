9449be64e969d218252626ec6f01dcf85754f476
ryuumugen <thangcp8@gmail.com>
2024-06-11 11:08:48 +0700

    update v 2.1.1 comment sửa schema

--- src/app/schemas/UserComment.js
+++ src/app/schemas/UserComment.js
@@ -1,8 +1,8 @@
 const Joi = require("joi");
 
 const commentSchema = Joi.object({
-  book_id: Joi.number().integer().max(11).required(),
-  user_id: Joi.number().integer().max(11).required(),
+  book_id: Joi.number().integer().required(),
+  user_id: Joi.number().integer().required(),
   content: Joi.string().required(), // TEXT, required
 });
 
