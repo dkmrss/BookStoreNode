d27b4e7d8ae46edd3ad790cc8c0d6e7402511959
ryuumugen <thangcp8@gmail.com>
2024-06-11 16:14:51 +0700

    update v 2.2.2

--- src/app/schemas/OrderSchema.js
+++ src/app/schemas/OrderSchema.js
@@ -1,13 +1,15 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const orderSchema = Joi.object({
   customer_id: Joi.number().integer().required(),
   name: Joi.string().required(),
-  phone: Joi.string().pattern(/^[0-9]{10}$/).required(),
-  address: Joi.string().allow(null, '').required(),
+  phone: Joi.string()
+    .pattern(/^[0-9]{10}$/)
+    .required(),
+  address: Joi.string().allow(null, "").required(),
   total: Joi.number().integer().required(),
-  note: Joi.string().allow(null, ''),
-  delivered: Joi.boolean().default(0),
+  note: Joi.string().allow(null, ""),
+  delivered: Joi.number().integer().default(0),
   method: Joi.number().integer().required(),
   payment: Joi.number().integer().required(),
 });
